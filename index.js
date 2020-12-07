41be3107c52c924cbb8a9d462a24627215dd04ef
yasith_sam <yasith.sam7@gmail.com>
2020-12-07 12:24:46 +0530

    js files added

---
+++ index.js
@@ -0,0 +1,25 @@
+function DisplayTime(){
+	var date=new Date();
+	var h=date.getHours();
+	var m=date.getMonth();
+	var s=date.getSeconds();
+	var session="AM";
+
+	if (h==0){
+		h=12;
+	}
+	if(h>12){
+		h=h-12;
+		session="PM";
+	}
+	h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(DisplayTime, 1000);
+
+}
